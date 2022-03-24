c0fcca51187f7245992644417dfe7c49198d54f6
Ram B <ram@livwell.asia>
2022-03-24 22:14:13 +0530

    s3

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,10 +1,16 @@
 export const environment = {
-    production: false,
-    API_BASE_PATH: "https://in-dev.livwell.asia/livwell/api/v1/",
-    API_KEY: "1234",
-    FINGER_PRINT: "Basic bGl2d2VsbDpsaXZ3ZWxsQDEyMw==",
-    MAP_API_KEY: "AIzaSyAWUoMZ4-OrIegyhv4z5nZBmgrI5A0IFWY"
-  };
+  s3: {
+    AWS_ACCESS_KEY: 'AKIASNBEQLTCOKURDA2M',
+    AWS_SECRET_KEY: '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',
+    REGION: 'AP_SOUTHEAST_1',
+    BUCKET_NAME: 'livwell/web'
+  },
+  production: false,
+  API_BASE_PATH: "https://in-dev.livwell.asia/livwell/api/v1/",
+  API_KEY: "1234",
+  FINGER_PRINT: "Basic bGl2d2VsbDpsaXZ3ZWxsQDEyMw==",
+  MAP_API_KEY: "AIzaSyAWUoMZ4-OrIegyhv4z5nZBmgrI5A0IFWY"
+};
 
 
-  
+
