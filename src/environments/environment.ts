34f37695f192d89890990a9164a1f4f4b9864c19
ram-livwell <ram@livwell.asia>
2023-04-25 19:24:47 +0700

    dist file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,36 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   s3: {
+//     AWS_ACCESS_KEY: 'AKIASNBEQLTCOKURDA2M',
+//     AWS_SECRET_KEY: 'UXcrGYQfyzVqRTVPZyTfTYo1SbUyFxXhLSFQhQgv',
+//     REGION: 'AP_SOUTHEAST_1',
+//     BUCKET_NAME: 'livwell/web'
+//   },
+//   // API_BASE_PATH: "http://localhost:3003/v1/",
+//   API_BASE_PATH: "https://in-dev.livwell.asia/livwell/api/v1/",
+//   API_KEY: "1234",
+//   FINGER_PRINT: "Basic bGl2d2VsbDpsaXZ3ZWxsQDEyMw==",
+//   MAP_API_KEY: "AIzaSyAWUoMZ4-OrIegyhv4z5nZBmgrI5A0IFWY"
+// };
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: false,
+  production: true,
   s3: {
     AWS_ACCESS_KEY: 'AKIASNBEQLTCOKURDA2M',
     AWS_SECRET_KEY: 'UXcrGYQfyzVqRTVPZyTfTYo1SbUyFxXhLSFQhQgv',
     REGION: 'AP_SOUTHEAST_1',
     BUCKET_NAME: 'livwell/web'
   },
-  // API_BASE_PATH: "http://localhost:3003/v1/",
-  API_BASE_PATH: "https://in-dev.livwell.asia/livwell/api/v1/",
-  API_KEY: "1234",
+
+  API_BASE_PATH: "https://in.api.livwell.asia/livwell/api/v1/",
+  API_KEY: '1234',
   FINGER_PRINT: "Basic bGl2d2VsbDpsaXZ3ZWxsQDEyMw==",
-  MAP_API_KEY: "AIzaSyAWUoMZ4-OrIegyhv4z5nZBmgrI5A0IFWY"
+  MAP_API_KEY: "AIzaSyCjREPYRmK1X1YWJDSonExO_pnmsLw617k"
 };
